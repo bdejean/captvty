31f435cd5c96183b07a8ddb0a7a80c66d21aac5e
Benoît Dejean <bdejean@gmail.com>
2015-06-14 09:22:38 +0200

    Move videos to /tmp/captvty.

--- start_captvty.sh
+++ start_captvty.sh
@@ -4,5 +4,8 @@ set -x
 
 xhost +local:
 
-exec docker run --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp:/home/luser/downloads captvty
+[ -d /tmp/captvty ] || mkdir /tmp/captvty
+chmod 2777 /tmp/captvty
+
+exec docker run --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp/captvty:/home/luser/downloads captvty
 
